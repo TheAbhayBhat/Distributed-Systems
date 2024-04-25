f50d7643f2b4dac7ce5d3d0b292b29fb2a3920ee
Harshvardhan Nishikant Patil <74046125+HarshvardhanP01@users.noreply.github.com>
2024-04-25 15:17:51 +0530

    HarshOP

--- 4.1 4.2 4.3 Berkeley Algorithm Java/C1.java
+++ 4.1 4.2 4.3 Berkeley Algorithm Java/C1.java
@@ -1,15 +1,22 @@
-
-import java.rmi.Naming;
+import java.rmi.*;
+import java.rmi.registry.*;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
+import java.time.format.*;
 
 public class C1 {
-    public static void main(String args[]){
+
+    public static void main(String[] args) {
+        
         try {
+            
             ServerImpl serverImpl = new ServerImpl(LocalTime.parse(args[1],DateTimeFormatter.ofPattern("HH:mm:ss")));
+
             Naming.rebind(args[0], serverImpl);
-        } catch (Exception ex) {
-            System.out.println("Execption at clock "+ args[0]+ ex);
+
+        } catch (Exception e) {
+            // TODO: handle exception
         }
     }
+    
+    
 }
--- 4.1 4.2 4.3 Berkeley Algorithm Java/ServerIntfc.java
+++ 4.1 4.2 4.3 Berkeley Algorithm Java/ServerIntfc.java
@@ -1,9 +1,10 @@
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.time.LocalTime;
 
-public interface ServerIntf extends Remote{
+public interface ServerIntfc extends Remote{
+    
     LocalTime getTime() throws RemoteException;
+
     void adjustTime(LocalTime serverTime, long diff) throws RemoteException;
 }
