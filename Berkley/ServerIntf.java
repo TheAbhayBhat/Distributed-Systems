082153a2cde540cd0fd593e9a3d53d683def7efa
Harshvardhan Nishikant Patil <74046125+HarshvardhanP01@users.noreply.github.com>
2024-04-25 10:30:20 +0530

    HarshOP

--- Berkley/ServerIntf.java
+++
@@ -1,9 +0,0 @@
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.time.LocalTime;
-
-public interface ServerIntf extends Remote{
-    LocalTime getTime() throws RemoteException;
-    void adjustTime(LocalTime serverTime, long diff) throws RemoteException;
-}
